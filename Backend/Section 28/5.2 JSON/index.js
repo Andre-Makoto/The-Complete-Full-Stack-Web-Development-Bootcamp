6c1bdf8869ea0b5aad5ec69502b6315d23221e2a
Andre-Makoto <andremolito4@gmail.com>
2025-06-16 16:31:15 -0300

    new if statement to see if fish was selected

--- Backend/Section 28/5.2 JSON/index.js
+++ Backend/Section 28/5.2 JSON/index.js
@@ -30,6 +30,11 @@ app.post("/recipe", (req, res) => {
       console.log(secondObject.name);
       res.render("index.ejs", {secondObject});
     };
+    if (choice === "fish"){
+      const thirdObject = data[2];
+      console.log(thirdObject.name);
+      res.render("index.ejs", {thirdObject});
+    }
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
 });
